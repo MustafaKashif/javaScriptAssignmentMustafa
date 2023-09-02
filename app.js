60f49cedcb873a68d0f058d773f04641136272b7
MustafaKashif <kashifmustafa822@gmail.com>
2023-09-02 06:04:27 +0500

    Mustafa Kashif

---
+++ app.js
@@ -0,0 +1,69 @@
+//! chapter No. 01
+// Question No. 01
+//? alert("Welcome to our website !");
+
+// Question No. 02
+//? alert("Error ! Please enter a valid Password.");
+
+// Question No. 03
+//? alert("Welcome to JS land......\n Happy coding");
+
+// Question No. 04
+//? alert("Welcome to JS Land.....");
+//? alert("Happy Coding ! ");
+
+// Question No. 05
+//? alert("Hello.... I can run JS through my web browser's Console")
+
+//! Chapter No. 01 Completed
+
+//! Chapter No. 02
+
+// Question No. 01
+//? var username;
+
+// Question No. 02
+//? var myName; // Variable declaration
+//? myName = "Ghulam-e-Mustafa"; //Assiging String to the variable or variable initialization
+//? alert(myName);
+
+// Question No. 03
+//? var message; // Variable declaration
+//? message = "Hello World !"; //Assiging String to the variable or variable initialization
+//? alert(message);
+
+// Question No. 04
+//? var studentName = "John Doe";
+//? var studentAge = "15 years old";
+//? var studentCertificate = "Certified Mobile Application development";
+//? alert(studentName);
+//? alert(studentAge);
+//? alert(studentCertificate);
+
+// Question No. 05
+//? var pizza = "Pizza";
+//? var p1 = "Pizz";
+//? var p2 = "Piz";
+//? var p3 = "Pi";
+//? var p4 = "P";
+//? alert(pizza +  "\n" + p1 + "\n" + p2 + "\n" + p3 + "\n" + p4);
+
+// Question No. 06
+//? var email; // Variable declaration
+//? email = "kashifmustafa822@gmail.com"; //Assiging String to the variable or variable initialization
+//? alert("My email Adress is " +email);
+
+// Question No. 07
+//? var book; // Variable declaration
+//? book = "A smarter way to learn javascript"; //Assiging String to the variable or variable initialization
+//? alert("I am trying to learn from the Book " +book);
+
+// Question No. 08
+//? document.write("Yah ! I can write HTML content through Javascript");
+
+// Question No. 09
+//? var design; // Variable declaration
+//? design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"; //Assiging String to the variable or variable initialization
+//? alert(design);
+
+//! Chapter No. 02 Completed
